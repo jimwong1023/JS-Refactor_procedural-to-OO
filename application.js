52b3f6dfe39cfed99c6eeb069d943b8ba28b0f1e
Jimmy <jimwong1023@gmail.com>
2013-09-10 16:11:24 -0700

    refactored JS to OO

--- application.js
+++ application.js
@@ -1,13 +1,26 @@
+function Die() {
+
+};
+
+Die.prototype.roll = function() {
+  $('.die').each(function(k, die) {
+    var value = Math.floor((Math.random()*6)+1);
+    $(die).text(value);
+  });
+};
+
+Die.prototype.add = function() {
+  $('.dice').append('<div class="die">0</div>');
+}
+
 $(document).ready(function() {
+  die = new Die()
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+     die.add()
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+     die.roll()
   });
 });
+
